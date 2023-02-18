ecb44cdcb280aee592d8078560ead6740f323543
shammikabir <shamimashammi861@gmail.com>
2023-02-18 17:17:48 +0600

    triangla calculation

--- js/calculation.js
+++ js/calculation.js
@@ -1,5 +1,27 @@
 document.getElementById('blog-btn').addEventListener('click',function(){
      window.location.href= 'question.html';
-    // console.log("button");
+     console.log("button");
 
 })
+document.getElementById('calculate-triangle').addEventListener('click',function(){
+     const base=document.getElementById("b").value;
+     const height=document.getElementById("h").value;
+    //  console.log(height,base);
+
+    // console.log("button");
+
+    const areabasecalculate=document.getElementById('calculate-base');
+    const areaheightcalculate=document.getElementById('calculate-height');
+    const areabaseset=areabasecalculate.innerText;
+    const areaheightset=areaheightcalculate.innerText;
+    // const areastring=parseFloat(areaset);
+    areaheightcalculate.innerText=height;
+    areabasecalculate.innerText=base;
+    // console.log(areaset);
+    const result= 0.5*base*height;
+    console.log(result);
+
+
+
+})
+
