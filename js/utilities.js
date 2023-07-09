3e20b8a04cfd50901044daae644f9ce3da35be9f
shammikabir <shamimashammi861@gmail.com>
2023-07-09 12:08:49 +0600

    first commit

--- js/utilities.js
+++ js/utilities.js
@@ -35,12 +35,6 @@ function displayArea(shapename,Arearesult){
 
 //  document.section.style.background=getRandomColor();
 
-function randomColor() {
-    let color = [];
-    for (let i = 0; i < 3; i++) {
-      color.push(Math.floor(Math.random() * 256));
-    }
-    return 'rgb(' + color.join(', ') + ')';
-  } 
+
   
   
